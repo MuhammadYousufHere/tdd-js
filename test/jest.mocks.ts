5e4f0f226b2268cb4d100213cfc974a33eedc069
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-09 19:13:11 +0500

    feat: jest setup

---
+++ test/jest.mocks.ts
@@ -0,0 +1,24 @@
+import { Logger } from 'pino'
+
+const mockLogger: Partial<Logger> = {
+  child: jest.fn().mockReturnThis(),
+  fatal: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+  debug: jest.fn(),
+  trace: jest.fn(),
+}
+
+jest.mock('pino-http', () => {
+  return jest.fn().mockImplementation(() => (): any => {
+    return {
+      logger: mockLogger,
+    }
+  })
+})
+
+const mockPino = jest.fn((): Logger => mockLogger as Logger) as any
+mockPino.transport = jest.fn()
+mockPino.destination = jest.fn()
+jest.mock('pino', () => mockPino)
