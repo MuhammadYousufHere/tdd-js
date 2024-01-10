ffd94b0639c637dc55c94a6299c2041f952a6e7c
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-10 20:08:47 +0500

    feat: app env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,1 +1,25 @@
-export const hello = 'hello'
+import 'dotenv/config'
+
+const {
+  DB_URL,
+  PORT,
+  API_GATEWAY_URL,
+  DB_TIMEOUT,
+  DB_USER,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  BASE_PATH,
+} = process.env
+
+export const config = {
+  dbUrl: DB_URL!,
+  port: PORT!,
+  apiGatewatUrl: API_GATEWAY_URL!,
+  dbTimeout: DB_TIMEOUT!,
+  dbUser: DB_USER!,
+  dbName: DB_NAME!,
+  dbPassword: DB_PASSWORD!,
+  dbPort: DB_PORT!,
+  apiBasePath: BASE_PATH!,
+}
