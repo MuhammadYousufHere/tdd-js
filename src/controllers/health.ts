51488c4739dddd2410b39ba6ec509e56257574c6
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-10 20:06:17 +0500

    feat: server health controller

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes'
 
 export function health(_req: Request, res: Response): void {
   res.status(StatusCodes.OK).json({
-    message: 'Auth service is healthy and OK.',
+    message: 'Auth service is up and running!',
     status: StatusCodes.OK,
   })
 }
