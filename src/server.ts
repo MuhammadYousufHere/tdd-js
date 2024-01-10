1a9b3642c6c8eab97a630111f7b616afeaac40fc
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-10 20:04:26 +0500

    feat: express server config

---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import { Application, urlencoded, json } from 'express'
+import http from 'http'
+import 'express-async-errors'
+import hpp from 'hpp'
+import helmet from 'helmet'
+import cors from 'cors'
+import compression from 'compression'
+import cookieParser from 'cookie-parser'
+import { Logger } from 'winston'
+
+import { config } from '@config/env'
+import { appRoutes } from '@/routes/routes'
+import { winstonLogger } from '@utils/logger'
+
+const log: Logger = winstonLogger('authenticationServer', 'debug')
+
+export function start(app: Application): void {
+  securityMiddleware(app)
+  standardMiddleware(app)
+  routesMiddleware(app)
+  startServer(app)
+}
+
+function standardMiddleware(app: Application): void {
+  app.use(compression())
+  app.use(json({ limit: '200mb' }))
+  app.use(urlencoded({ extended: true, limit: '200mb' }))
+  app.use(cookieParser())
+}
+
+function routesMiddleware(app: Application): void {
+  appRoutes(app)
+}
+
+function securityMiddleware(app: Application) {
+  app.set('trust proxy', 1)
+  app.disable('etag')
+  app.use(hpp())
+  app.use(helmet())
+  app.use(
+    cors({
+      origin: config.apiGatewatUrl,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    })
+  )
+}
+
+function startServer(app: Application): void {
+  try {
+    const httpServer: http.Server = new http.Server(app)
+    log.info(`Authentication server has started with process id ${process.pid}`)
+    httpServer.listen(config.port, () => {
+      log.info(`Auth service running on port ${config.port}`)
+    })
+  } catch (error) {
+    log.error('error', 'Auth Service startServer() method error:', error)
+  }
+}
