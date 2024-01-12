853e41691f253d786f3b0663635484f92cbc3e12
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-12 20:11:38 +0500

    wip: auth middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,38 @@
+import { config } from '@/config/env'
+import { IAuthPayload } from '@/interfaces/auth'
+import { NotAuthorizedError } from '@/utils/errorHandler'
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+import { jwtDecode } from 'jwt-decode'
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // verify token
+  try {
+    const cookie = req.cookies[config.authCookieName]
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.split(' ')[1]
+      console.log({ decode: jwtDecode(token) })
+      if (!token) {
+        throw new NotAuthorizedError(
+          'Not Authenticated',
+          'from authMiddleware() method'
+        )
+      }
+
+      //   const payload: IAuthPayload = verify(
+      //     token,
+      //     config.jwtSecret!
+      //   ) as IAuthPayload
+
+      //   req.currentUser = payload
+    }
+
+    next()
+  } catch (error) {
+    res.status(401).json({ msg: 'Token is not valid!' })
+  }
+}
