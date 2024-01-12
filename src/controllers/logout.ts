b38dc02ba5be5a92b2f4df60b63145d144920e1c
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-12 20:13:11 +0500

    wip: logout enpoint

---
+++ src/controllers/logout.ts
@@ -0,0 +1,37 @@
+import { config } from '@/config/env'
+import { AuthModel } from '@/models/auth'
+import { BadRequestError } from '@/utils/errorHandler'
+import { winstonLogger } from '@/utils/logger'
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { Logger } from 'winston'
+
+const log: Logger = winstonLogger('authenticationServer', 'debug')
+
+export async function logout(req: Request, res: Response): Promise<void> {
+  try {
+    await AuthModel.update(
+      {
+        loginStatus: 'inactive',
+        lastLogin: new Date(),
+      },
+      { where: { id: req.currentUser?.id } }
+    )
+    res.clearCookie(config.jwtSecret).json({
+      code: StatusCodes.OK,
+      message: 'Sign out successful',
+    })
+  } catch (error) {
+    if (error instanceof BadRequestError) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: error.message, code: error.code })
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: 'Something went wrong',
+        code: StatusCodes.INTERNAL_SERVER_ERROR,
+      })
+    }
+    log.error('from logout()', error)
+  }
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,13 @@
+import { logout } from '@/controllers/logout'
+import {
+  changePassword,
+  forgotPassword,
+  resetPassword,
+} from '@/controllers/password'
 import { read } from '@/controllers/signin'
 import { create } from '@/controllers/signup'
+import { verifyEmail } from '@/controllers/verifyEmail'
+import { authMiddleware } from '@/middleware/auth.middleware'
 import express, { Router } from 'express'
 import multer from 'multer'
 
@@ -15,5 +23,10 @@ const router: Router = express.Router()
 export function authRoutes(): Router {
   router.post('/signup', upload.single('profilePicture'), create)
   router.post('/signin', read)
+  router.put('/forgot-password', forgotPassword)
+  router.put('/verify-email', verifyEmail)
+  router.put('/reset-password/:token', resetPassword)
+  router.put('/change-password', authMiddleware, changePassword)
+  router.delete('/logout', authMiddleware, logout)
   return router
 }
