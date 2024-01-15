ae3d5591e28859caa4b00bb169dcbe430e81c0b1
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-15 20:08:43 +0500

    feat: auth middleware complete

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,36 +1,62 @@
 import { config } from '@/config/env'
 import { IAuthPayload } from '@/interfaces/auth'
-import { NotAuthorizedError } from '@/utils/errorHandler'
+import { BadRequestError, NotAuthorizedError } from '@/utils/errorHandler'
 import { Request, Response, NextFunction } from 'express'
+import { StatusCodes } from 'http-status-codes'
 import { verify } from 'jsonwebtoken'
-import { jwtDecode } from 'jwt-decode'
 
 export function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  // verify token
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith('Bearer ')
+  ) {
+    throw new BadRequestError(
+      'No Bearer token in the Authorization header. Make sure you authorize your request by providing the HTTP header',
+      'authMiddlware()'
+    )
+  }
+  if (!req.signedCookies[config.authCookieName]) {
+    throw new NotAuthorizedError(
+      'It looks like you are not logged in',
+      'from authMiddleware() method'
+    )
+  }
   try {
-    if (req.headers.authorization) {
-      const token = req.headers.authorization.split(' ')[1]
-      if (!token) {
-        throw new NotAuthorizedError(
-          'Not Authenticated',
-          'from authMiddleware() method'
-        )
-      }
-
-      //   const payload: IAuthPayload = verify(
-      //     token,
-      //     config.jwtSecret!
-      //   ) as IAuthPayload
-
-      //   req.currentUser = payload
+    const authHeader = req.headers.authorization.split(' ')
+    const token = authHeader[1]
+    if (!token) {
+      throw new NotAuthorizedError(
+        'Not Authenticated',
+        'from authMiddleware() method'
+      )
     }
+    const payload: IAuthPayload = verify(
+      token,
+      config.jwtSecret!
+    ) as IAuthPayload
+
+    if (!payload) {
+      res.status(StatusCodes.UNAUTHORIZED).json({
+        message: 'Access token has expired',
+        code: 401,
+      })
+    }
+    req.currentUser = payload
 
     next()
   } catch (error) {
-    res.status(401).json({ msg: 'Token is not valid!' })
+    if (error instanceof NotAuthorizedError) {
+      res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: error.message, code: error.code })
+    }
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: 'Something went wrong!',
+      code: StatusCodes.INTERNAL_SERVER_ERROR,
+    })
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -19,8 +19,6 @@ import { config } from '@config/env'
 import { appRoutes } from '@/routes/routes'
 import { winstonLogger } from '@utils/logger'
 import { CustomError, IErrorResponse } from './utils/errorHandler'
-import { IAuthPayload } from './interfaces/auth'
-import { verify } from 'jsonwebtoken'
 
 const log: Logger = winstonLogger('authenticationServer', 'debug')
 
@@ -36,7 +34,7 @@ function standardMiddleware(app: Application): void {
   app.use(compression())
   app.use(json({ limit: '200mb' }))
   app.use(urlencoded({ extended: true, limit: '200mb' }))
-  app.use(cookieParser())
+  app.use(cookieParser(config.authCookieSecret))
 }
 
 function routesMiddleware(app: Application): void {
@@ -55,18 +53,6 @@ function securityMiddleware(app: Application) {
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     })
   )
-  // app.use((req: Request, _res: Response, next: NextFunction) => {
-  //   if (req.headers.authorization) {
-  //     const token = req.headers.authorization.split(' ')[1]
-  //     console.log({ token })
-  //     const payload: IAuthPayload = verify(
-  //       token,
-  //       config.jwtSecret!
-  //     ) as IAuthPayload
-  //     req.currentUser = payload
-  //   }
-  //   next()
-  // })
 }
 function authErrorHandler(app: Application): void {
   app.use(
