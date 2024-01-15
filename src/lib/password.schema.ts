1472bae8492df2b94e0e12256e86cc284b8f8007
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-15 20:11:04 +0500

    feat: password validation schema

--- src/lib/password.schema.ts
+++ src/lib/password.schema.ts
@@ -26,17 +26,20 @@ const changePasswordSchema: ObjectSchema = Joi.object().keys({
   newPassword: Joi.string()
     .min(8)
     .max(25)
-    .regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,15}$/)
+    .regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,}$/)
     .required()
     .messages({
-      'string.base': 'Password must be of type string',
-      'string.min': 'password should not be less than 8 characters',
-      'string.max': 'password should not exceed more than 15 characters',
-      'string.empty': 'Password is a required field',
+      'string.base': 'new password must be of type string',
+      'string.min': 'new password should not be less than 8 characters',
+      'string.max': 'new password should not exceed more than 15 characters',
+      'string.empty': 'new password is a required field',
       'string.pattern.base':
-        'Password should contain at least one number, one lowercase and uppercase letter and one special character',
+        'new password should contain at least one number, one lowercase and uppercase letter and one special character',
     }),
-  oldPassword: Joi.string().required(),
+  oldPassword: Joi.string().required().messages({
+    'string.base': 'old password must be of type string',
+    'string.empty': 'old password is a required field',
+  }),
 })
 
 export { resetPasswordSchema, changePasswordSchema, emailSchema }
