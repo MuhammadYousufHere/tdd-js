31b15bcd7b528b2506f22d4416b3ac9a0577a917
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-15 20:12:22 +0500

    feat: curr user feature - refresh token complete

--- src/routes/currUser.ts
+++ src/routes/currUser.ts
@@ -1,4 +1,3 @@
-import { currentUser } from '@/controllers/currentUser'
 import { refreshToken } from '@/controllers/refreshToken'
 import { resendEmail } from '@/controllers/verifyEmail'
 import express, { Router } from 'express'
@@ -7,7 +6,6 @@ const router: Router = express.Router()
 
 export function currentUserRoutes(): Router {
   router.get('/refresh-token', refreshToken)
-  router.get('/currrent-user', currentUser)
   router.post('/resend-email', resendEmail)
 
   return router
